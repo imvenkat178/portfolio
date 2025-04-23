e04b3caac19d9f8819d6ccece36bd6a9c36f0f3c
imvenkat178 <imvenkatareddy.b@gmail.com>
2025-04-22 21:13:42 -0400

    Changed the headers

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,7 +1,7 @@
 const portfolioData = {
     profile: {
       name: "Venkata Reddy Bhimireddy",
-      title: "SOFTWARE ENGINEER / FULL STACK DEVELOPER ",
+      title: "SOFTWARE ENGINEER",
       email: "bhimireddyvenkatreddy2@gmail.com",
       phone: "(317) 419-8533",
       github: "GitHub",
